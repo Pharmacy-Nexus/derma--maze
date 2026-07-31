2887c64c0996e388a2829cf9a31e7237ff3636de
Ahmed Nabil <nobel4530@gmail.com>
2026-07-31 12:10:40 +0300

    Add files via upload

--- js/core/site-config.js
+++ js/core/site-config.js
@@ -1,12 +1,12 @@
 window.DM_SITE_CONFIG = Object.freeze({
-  version: '6.2.0',
+  version: '6.3.1',
   production: true,
   canonicalBase: 'https://pharmacy-nexus.github.io/derma-maze/',
   order: {
-    enabled: false,
-    whatsappNumber: '',
-    messageAr: 'مرحباً، أريد معرفة تفاصيل كتاب Derma-Maze',
-    messageEn: 'Hello, I would like to know more about the Derma-Maze book.'
+    enabled: true,
+    whatsappNumber: '201211361734',
+    messageAr: 'مرحباً، أريد طلب كتاب Derma-Maze Volume 1 ومعرفة تفاصيل التوصيل.',
+    messageEn: 'Hello, I would like to order Derma-Maze Volume 1 and know the delivery details.'
   },
   study: {
     defaultDailyTarget: 20
